46a107a8677f7dba0307990c4b1605558ffa392b
VuTran2k3 <125528481+VuTran2k3@users.noreply.github.com>
2025-09-25 22:52:29 +0700

    25/09/2025 update student

--- src/main/java/com/example/stdportal/Model/Classroom.java
+++ src/main/java/com/example/stdportal/Model/Classroom.java
@@ -9,18 +9,19 @@ import org.springframework.data.annotation.CreatedDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+@Table(name = "classroom")
 @Entity
 @Getter @Setter @NoArgsConstructor
 public class Classroom {
     @Id
     private String name;
 
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "teacher_id")
-//    private Teacher homeroomTeacher;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "teacher_id")
+    private Teacher homeroomTeacher;
 
-//    @OneToMany(mappedBy = "classroom", cascade = CascadeType.ALL, orphanRemoval = true)
-//    private List<Student> students = new ArrayList<>();
+    @OneToMany(mappedBy = "classroom", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Student> students = new ArrayList<>();
 
     private String meta;
     @CreatedDate
